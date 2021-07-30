285991eece2bc9acb324ed5668de12e8859c0f42
Vince Carusca <vincecarusca@gmail.com>
2021-07-30 18:19:22 +0800

    enter

--- metroevents1/settings.py
+++ metroevents1/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
