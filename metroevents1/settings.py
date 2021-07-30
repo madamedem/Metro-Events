23241fa91e1511087fce33dab52c983f07c8cf82
Vince Carusca <vincecarusca@gmail.com>
2021-07-30 17:59:59 +0800

    enter

--- metroevents1/settings.py
+++ metroevents1/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'metroevents1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metroevents_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
+
 
 
 # Password validation
