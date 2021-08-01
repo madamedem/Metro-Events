06fbfdb2aa8f62dd0e347de634ca20292f2f8513
Vince Carusca <vincecarusca@gmail.com>
2021-08-02 00:12:24 +0800

    Update urls.py

--- organizer/urls.py
+++ organizer/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'organizer'
 
 urlpatterns = [
+    path('organizerHome', views.organizerHomeView.as_view(), name='organizer_home'),
     path('organizerEvents', views.organizerEventsView.as_view(), name='organizer_events'),
     path('organizerReview', views.organizerReviewView.as_view(), name='organizer_review'),
     path('organizerProfile', views.organizerProfileView.as_view(), name='organizer_profile'),
     path('organizerLogin', views.organizerLoginView.as_view(), name='organizer_login'),
-    path('organizerHome/<username>', views.organizerLoggedInView.as_view(), name="organizer_account"),
+    path('organizerHome/<uname>', views.organizerLoggedInView.as_view(), name="organizer_homeaccount"),
 ]
