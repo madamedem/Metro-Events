59386144ac1881a8acd09ceb11c6050aae7f5221
Vince Carusca <vincecarusca@gmail.com>
2021-08-01 22:46:44 +0800

    Update views.py

--- organizer/views.py
+++ organizer/views.py
@@ -4,22 +4,63 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import View
 from django.contrib import messages
+from django.contrib.auth.models import auth
+from django.contrib.auth import login, logout
+
+from .forms import OrganizerForm, LoginForm
+from .models import Account, Organizer
 
 
 
 # Create your views here.
 
-class organizerHomeView(View):
-    template_name="organizer-dashboard.html"
-
-    def get(self,request):
-        return render(request,self.template_name)
-
 class organizerLoginView(View):
     template_name="organizer-login.html"
 
     def get(self,request):
-        return render(request,self.template_name)
+        formLogin = LoginForm()
+        return render(request, self.template_name, {'formLogin': formLogin})
+
+    def post(self, request):
+        formLogin = LoginForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print("Username:" + username)
+        error = ""
+        print("No of record:" + str(Account.objects.filter(pk=username).count()))
+        
+
+        if Account.objects.filter(pk=username).count() != 0:
+            account = Account.objects.get(pk=username)
+            if account.password == password:
+                print("username" + username)
+
+                return redirect(reverse('organizer:organizer_home', kwargs={'username': username, }))
+            else:
+                error = "Incorrect password."
+        else:
+            error = "Username does not exist."
+
+        return render(request, self.template_name, {'formLogin': formLogin, 'error': error})
+
+class organizerHomeView(View):
+    template_name="organizer-dashboard.html"
+
+    def get(self,request):
+        return render(request,self.template_name)      
+
+ 
+class organizerloggedInView(View):
+    template_name = "organizer-dashboard.html"
+
+    def get(self, request, username):
+
+        print(username)
+        return render(request, self.template_name, {'username': username})
+
+
+
+
 
 class organizerEventsView(View):
     template_name="organizer-events.html"
