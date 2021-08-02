0698eb8f3e31b9d6ef80e3babca42feb45fe035e
Vince Carusca <vincecarusca@gmail.com>
2021-08-02 15:33:05 +0800

    enter

--- organizer/forms.py
+++ organizer/forms.py
@@ -37,14 +37,3 @@ class OrganizerForm(forms.ModelForm):
         widget=forms.RadioSelect,
         choices=GENDER_CHOICES,
     )
-
-class EventCreationForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ('event_name', 'event_description', 'event_type', 'start_date', 'end_date',
-                  'start_time', 'end_time')
-
-class ReviewCreationForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = ('title', 'content', 'rating')
